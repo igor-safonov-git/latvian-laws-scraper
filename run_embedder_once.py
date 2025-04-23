4aa4623b6940860602a6409b43f758b97fddfb94
igor-safonov-git <safonoff@gmail.com>
2025-04-23 14:55:39 +0300

    Add script to run embedder once for testing

---
+++ run_embedder_once.py
@@ -0,0 +1,10 @@
+#\!/usr/bin/env python3
+"""
+Script to run the embedder once for testing.
+"""
+import asyncio
+from embedder_optimized import OptimizedEmbedderService, run_once
+
+if __name__ == "__main__":
+    asyncio.run(run_once())
+EOF < /dev/null
